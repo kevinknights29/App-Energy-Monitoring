852aedde052ff029056c8434b7a9536f0a319617
Kevin Knights <kevin.khadir@dell.com>
2022-11-13 11:10:24 -0500

    updated app settings and urls

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "backend",
+    "api",
+    "rest_framework",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
